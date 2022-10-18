d1bb8f0e508aad755f20dfb6c2d16b0d428d1fba
Frederic Fleisch <volkramer@gmail.com>
2022-10-18 12:07:04 +0200

    add Main script; add game over and new game method

---
+++ Main.cs
@@ -0,0 +1,31 @@
+using Godot;
+
+public class Main : Node
+{
+    [Export]
+    public PackedScene MobScene;
+
+    public int Score;
+
+    public override void _Ready()
+    {
+        GD.Randomize();
+    }
+
+    public void GameOver()
+    {
+        GetNode<Timer>("MobTimer").Stop();
+        GetNode<Timer>("ScoreTimer").Stop();
+    }
+
+    public void NewGame()
+    {
+        Score = 0;
+
+        var player = GetNode<Player>("Player");
+        var startPosition = GetNode<Position2D>("StartPosition");
+        player.Start(startPosition.Position);
+
+        GetNode<Timer>("StartTimer").Start();
+    }
+}
